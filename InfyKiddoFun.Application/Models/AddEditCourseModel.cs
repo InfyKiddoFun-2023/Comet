e9db67dc9d0eced410891639107726476efe570e
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-09 20:15:48 +0530

    Created Models folder and AddEditCourseModel class

---
+++ InfyKiddoFun.Application/Models/AddEditCourseModel.cs
@@ -0,0 +1,17 @@
+﻿using InfyKiddoFun.Domain.Enums;
+
+namespace InfyKiddoFun.Application.Models;
+
+public class AddEditCourseModel
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+
+    public AgeGroup AgeGroup { get; set; }
+
+    public DifficultyLevel DifficultyLevel { get; set; }
+
+    public SpecificStream SpecificStream { get; set; }
+}
