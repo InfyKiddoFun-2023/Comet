1b57ee15d99be90c5b4f514efd37f7c72afa98ea
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-09 20:16:40 +0530

    Created Features folder and CourseService class

---
+++ InfyKiddoFun.Application/Features/CourseService.cs
@@ -0,0 +1,31 @@
+﻿using InfyKiddoFun.Application.Interfaces;
+using InfyKiddoFun.Application.Models;
+using InfyKiddoFun.Domain.Entities;
+using InfyKiddoFun.Infrastructure;
+
+namespace InfyKiddoFun.Application.Features;
+
+public class CourseService : ICourseService
+{
+    private readonly AppDbContext _appDbContext;
+
+    public CourseService(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    
+    public void AddCourse(AddEditCourseModel model)
+    {
+        var course = new Course()
+        {
+            AgeGroup = model.AgeGroup,
+            Duration = model.Duration,
+            Description = model.Description,
+            DifficultyLevel = model.DifficultyLevel,
+            Name = model.Name,
+            SpecificStream = model.SpecificStream
+        };
+        _appDbContext.Courses.Add(course);
+        _appDbContext.SaveChanges();
+    }
+}
