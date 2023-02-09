170943d8e146b45baec71e5868062cb6b9437078
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-09 18:15:27 +0530

    Added basic middleware setup and comments

--- InfyKiddoFun.API/Program.cs
+++ InfyKiddoFun.API/Program.cs
@@ -1,11 +1,24 @@
 using InfyKiddoFun.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
+// Create WebApplicationBuilder object
 var builder = WebApplication.CreateBuilder(args);
+
+// Register all services to the IoC container
 var connectionString = builder.Configuration.GetConnectionString("Connection1");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Build WebApplication object
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure middlewares
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 
+// Run the WebApplication
 app.Run();
