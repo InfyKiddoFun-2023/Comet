9aba1bb09719eb383b0455d0435d5ab89572dad6
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-09 20:17:39 +0530

    Created Controllers folder and new Controller: CourseController

---
+++ InfyKiddoFun.API/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using InfyKiddoFun.Application.Interfaces;
+using InfyKiddoFun.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfyKiddoFun.API.Controllers;
+
+[Route("api/courses")]
+[ApiController]
+public class CourseController : ControllerBase
+{
+    private readonly ICourseService _courseService;
+
+    public CourseController(ICourseService courseService)
+    {
+        _courseService = courseService;
+    }
+
+    [HttpPost("add")]
+    public IActionResult AddCourse(AddEditCourseModel model)
+    {
+        _courseService.AddCourse(model);
+        return Ok();
+    }
+    
+}
