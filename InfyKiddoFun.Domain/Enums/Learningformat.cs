61a8d611954a6426159a3f9a9a4ddc982ae98cca
MUTHUGANESAN N G (RA2111003020343) <mn3258@srmist.edu.in>
2023-02-11 18:18:27 +0530

    Created LearningFormat enum

---
+++ InfyKiddoFun.Domain/Enums/Learningformat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfyKiddoFun.Domain.Enums
+{
+    public enum Learningformat
+    {
+        Videolessons,
+        Interactivesimulations,
+        Writtentext
+
+    }
+}
+	
