671934bb75811ef73635ce8e449e5b398e5772da
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-16 20:12:00 +0530

    Update Enrollment entity

--- InfyKiddoFun.Domain/Entities/Enrollment.cs
+++ InfyKiddoFun.Domain/Entities/Enrollment.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace InfyKiddoFun.Domain.Entities;
 
-namespace InfyKiddoFun.Domain.Entities
+public class Enrollment
 {
-    public class Enrollment
-    {
-        public int EnrollmentId { get; set; }
-        public string CourseId { get; set; }
-        public string StudentId { get; set; }
+    public string Id { get; set; }
+    public string CourseId { get; set; }
+    public string StudentId { get; set; }
 
 
-        public Course Course { get; set; }
-        public StudentUser Student { get; set; }
-    }
+    public Course Course { get; set; }
+    public StudentUser Student { get; set; }
 }
