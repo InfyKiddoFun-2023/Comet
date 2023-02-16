5028d11f9781deeb3015d4be9ee8abe03070b5e8
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-02-16 20:22:36 +0530

    Added new models for Identity

---
+++ InfyKiddoFun.Application/Models/Identity/LoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace InfyKiddoFun.Application.Models.Identity;
+
+public class LoginRequest
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ InfyKiddoFun.Application/Models/Identity/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace InfyKiddoFun.Application.Models.Identity;
+
+public class LoginResponse
+{
+    public string Token { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryDate { get; set; }
+}
