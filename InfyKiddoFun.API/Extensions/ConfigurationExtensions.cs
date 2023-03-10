a4c6916f7ff003fed86bdc7090075f4e3ec86b82
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-03-10 06:09:58 +0530

    Created ConfigurationExtensions

---
+++ InfyKiddoFun.API/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using InfyKiddoFun.Domain.Configurations;
+
+namespace InfyKiddoFun.API.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static TokenConfiguration GetTokenConfiguration(this IConfiguration configuration)
+    {
+        return configuration.GetSection(nameof(TokenConfiguration)).Get<TokenConfiguration>();
+    }
+}
