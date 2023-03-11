504f13a32a5e0a44db74aa8c439292d2c2f221db
Sathiyaraman <Sathiyaraman2003@gmail.com>
2023-03-11 12:55:48 +0530

    Added new extension classes

---
+++ InfyKiddoFun.Application/Extensions/QueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using InfyKiddoFun.Domain.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfyKiddoFun.Application.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize) where T : class
+    {
+        if (source == null) throw new Exception("Empty data source!");
+        pageNumber = pageNumber == 0 ? 1 : pageNumber;
+        pageSize = pageSize == 0 ? 10 : pageSize;
+        var count = await source.CountAsync();
+        pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
+    }
+}
